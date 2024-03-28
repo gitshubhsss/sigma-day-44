ce89c72c6578aa5193a9e90856bf822e7a889234
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-28 12:59:14 +0530

    use of append method

--- practice/practice3.js
+++ practice/practice3.js
@@ -1,20 +1,17 @@
 // a <div> with a black border and pink background color with the following elements inside of it :
 //  1: another <h1> that says I am in div
 //  2: a <p>that says "MEE TOO!"
-
 let div=document.createElement('div');
-div.style.border="2px solid black";
-div.style.backgroundColor="pink";
+let h1=document.createElement('h1');
+let p=document.createElement('p');
+
+h1.innerText="I am in div";
+p.innerText="MEE TOO";
 
 let body=document.querySelector('body');
-body.appendChild(div);
+body.append(div);
 
-let h1=document.createElement('h1');
-h1.innerText="I am in div";
-div.insertAdjacentElement('afterbegin',h1)
+div.append(h1)
+div.append(p);
 
-let p=document.createElement('p');
-p.innerText="MEE TOO!";
-
-div.insertAdjacentElement('beforeend',p)
-
+div.classList.add('box')
