f8d3a9d81a70955606186b9a204e3f7cb6fbda2a
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-28 13:17:56 +0530

    Add the following attributes to the element:         -change placeholder value of input to 'username';         -change the id of button to 'btn'

---
+++ practice/Assigments/Assigment2.js
@@ -0,0 +1,5 @@
+let userName=document.querySelector('input');
+userName.setAttribute("placeholder","shubham")//we have use the set attributes method over here
+
+let btn=document.querySelector('button');
+btn.setAttribute("id","btn")//now the id will become #btn
